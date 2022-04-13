17591f8f4ad8839e47bb9481d77e765622ed7fe3
Mohitb1426 <rawatmohituk12@gmail.com>
2022-04-13 12:32:31 +0530

    server side rendering

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
-
+import { Provider } from "react-redux";
+import { store } from "../src/reduxToolkit/Store";
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import App from '../src/App';
@@ -15,10 +16,13 @@ app.use('^/$', (req, res, next) => {
             return res.status(500).send("Some error happened")
         }
 
-        return res.send(data.replace('<div id="root"></div>', `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`))
+        return res.send(data.replace('<div id="root"></div>', `<div id="root">${ReactDOMServer.renderToString(
+            <Provider store={store}>
+                <App />
+            </Provider>)}</div>`))
     })
 })
-app.use(express.static(path.resolve(__dirname,'..','build')));
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.listen(PORT, () => {
     console.log(`App launched on ${PORT}`);
